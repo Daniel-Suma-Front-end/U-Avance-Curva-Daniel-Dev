c9ada9ad51e133d7f6f7838aec0abcaf03c68704
Daniel Lopez <daniel94lad@gmail.com>
2020-08-15 18:14:44 -0500

    endOfSeccion3

--- src/index.js
+++ src/index.js
@@ -4,31 +4,35 @@ import faker from 'faker';
 // Componentes
 
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () =>{
     return (
         <div className="ui container comments">
-            <CommentDetail 
-            author= "Edgar" 
-            timeAgo = "Today at 6:00pm" 
-            text="Last Saying something wins"
-            avatar={faker.image.avatar()}
-             />
-
-            <CommentDetail 
-            author= "Suma" 
-            timeAgo = "Today at 3:00pm" 
-            text="No man alive say a word"
-            avatar={faker.image.avatar()}
-            />
-
-            <CommentDetail 
-            author= "Teller" 
-            timeAgo = "Yesterday at 5:00pm" 
-            text="Dead men dont tell stories" 
-            avatar={faker.image.avatar()}
-            />
-
+            <ApprovalCard>
+                <CommentDetail 
+                author= "Edgar" 
+                timeAgo = "Today at 6:00pm" 
+                text="Last Saying something wins"
+                avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author= "Suma" 
+                timeAgo = "Today at 3:00pm" 
+                text="No man alive say a word"
+                avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author= "Teller" 
+                timeAgo = "Yesterday at 5:00pm" 
+                text="Dead men dont tell stories" 
+                avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 }
