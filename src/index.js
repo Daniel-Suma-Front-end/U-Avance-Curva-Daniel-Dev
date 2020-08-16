50149f75df12234b520d9e67e3e27bc7ac39424a
Daniel Lopez <daniel94lad@gmail.com>
2020-08-15 19:01:08 -0500

    'seccion4Inicio'

--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-// Componentes
-
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard';
 
 const App = () =>{
     return (
-        <div className="ui container comments">
-            <ApprovalCard>
-                <CommentDetail 
-                author= "Edgar" 
-                timeAgo = "Today at 6:00pm" 
-                text="Last Saying something wins"
-                avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author= "Suma" 
-                timeAgo = "Today at 3:00pm" 
-                text="No man alive say a word"
-                avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail 
-                author= "Teller" 
-                timeAgo = "Yesterday at 5:00pm" 
-                text="Dead men dont tell stories" 
-                avatar={faker.image.avatar()}
-                />
-            </ApprovalCard>
+        <div>
+            Hi there!
         </div>
     )
 }
