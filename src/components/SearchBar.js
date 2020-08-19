f6b9baec803f551f2cedc8645738a2604cdd6044
Daniel Lopez <daniel94lad@gmail.com>
2020-08-18 21:35:01 -0500

    S7E86

---
+++ src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+    state ={term:'',leyenda:'Search'}
+    render(){
+        return(
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input 
+                        type="text" 
+                        value={this.state.term} 
+                        placeholder={this.state.leyenda}
+                        onChange={(e)=> this.setState({term:e.target.value})}
+                        />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
